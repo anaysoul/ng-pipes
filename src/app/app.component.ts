e4bba77a35a18d68e1c6e409766f00f38fe9b6f5
anaysoul <soulyana_lakew@outlook.com>
2022-06-04 12:41:29 -0400

    displaying JSON

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,13 @@ export class AppComponent {
   date: string = '';
   amount: number = 0.0;
   height: number = 0;
+  miles: number = 0;
+
+  car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2000,
+  };
 
   onNameChange(value: string) {
     this.name = value;
@@ -25,5 +32,9 @@ export class AppComponent {
 
   onHeightChange(value: number) {
     this.height = value;
+  }
+
+  onMilesChange(value: number) {
+    this.miles = value;
   }
 }
